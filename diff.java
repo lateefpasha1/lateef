bd1f1129e4333e27e3db9a8b0661b17b07ea4024
lateefpasha1 <144432663+lateefpasha1@users.noreply.github.com>
2023-09-08 14:06:19 +0530

    Add files via upload

---
+++ diff.java
@@ -0,0 +1,16 @@
+// Importing required classes
+import java.io.*;
+
+// Main class
+class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Print statement
+		System.out.println(
+			"Welcome to GeeksforGeeks Learning");
+            System.out.println(
+			"Welcome to GeeksforGeeks Learning");
+	}
+}
